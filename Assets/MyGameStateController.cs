05fbe2049de80d64307ae245a1e766cfbfb5ad76
Ctomes <72369162+Ctomes@users.noreply.github.com>
2023-02-24 14:55:36 -0800

    rabbit bots

---
+++ Assets/MyGameStateController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameStateController : MonoBehaviour
+{
+    int turn = 0;
+    int[] hip = new int[] { 1, 2, 4};
+    int[] hop = new int[] { 1, 3, 4};
+    int[] hippity = new int[] { 1, 2, 4, 5, 6, 7, 8};
+    int[] hoppity = new int[] { 1, 3, 4, 5, 6, 7, 8};
+    int[] hippy = new int[] { 1, 2, 4 , 5, 8};
+    int[] hoppy = new int[] { 1, 3, 4 , 5, 8};
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PrintRabbitLocations()
+    {
+        Debug.Log("Hip is in room: " + hip[turn % (hip.Length)].ToString());
+        Debug.Log("Hopp is in room: " + hop[turn % (hop.Length)].ToString());
+        Debug.Log("Hippity is in room: " + hippity[turn % (hippity.Length)].ToString());
+        Debug.Log("Hoppity is in room: " + hoppity[turn % (hoppity.Length)].ToString());
+        Debug.Log("Hippy is in room: " + hippy[turn % (hippy.Length)].ToString());
+        Debug.Log("Hoppy is in room: " + hoppy[turn % (hoppy.Length)].ToString());
+
+    }
+    public void IncrementTurn()
+    {
+        turn += 1;
+        Debug.Log("Turn is now: " + turn.ToString());
+        PrintRabbitLocations();
+    }
+}

3d6947064250dbf2381f0c15202b5a20934845b1
Ctomes <72369162+Ctomes@users.noreply.github.com>
2023-03-03 13:57:05 -0800

    rabbit mechanics
    pushh

--- Assets/MyGameStateController.cs
+++ Assets/MyGameStateController.cs
@@ -2,24 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MyGameStateController : MonoBehaviour
 {
     int turn = 0;
+    //The values represent the rooms in which the rabbit will be in during that given turn.
     int[] hip = new int[] { 1, 2, 4};
     int[] hop = new int[] { 1, 3, 4};
     int[] hippity = new int[] { 1, 2, 4, 5, 6, 7, 8};
     int[] hoppity = new int[] { 1, 3, 4, 5, 6, 7, 8};
     int[] hippy = new int[] { 1, 2, 4 , 5, 8};
     int[] hoppy = new int[] { 1, 3, 4 , 5, 8};
+    public GameObject rabbit;
+    public GameObject hop_rabbit;
+    public GameObject hippity_rabbit;
+    public GameObject hoppity_rabbit;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Starting");
+        SpawnRabbits(hip);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    void SpawnRabbits(int[] hip)
+    {
+        Debug.Log("Spawning rabbit");
+        rabbit = Instantiate(rabbit, transform.position, transform.rotation);
         
     }
     void PrintRabbitLocations()
@@ -37,5 +50,10 @@ public class MyGameStateController : MonoBehaviour
         turn += 1;
         Debug.Log("Turn is now: " + turn.ToString());
         PrintRabbitLocations();
+        if(turn%2==0){
+            rabbit.transform.position = new Vector3(100.0f, 100.0f, 1.0f);
+            }else{
+                rabbit.transform.position = transform.position;
+            }
     }
 }
